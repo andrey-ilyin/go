c49d72d9dd70abbcfca172d77c5274301761a727
Andrey Ilyin <andrey.ilyin@ispringsolutions.com>
2017-03-15 16:44:49 +0300

    1. Constants have been moved to package level. 2. Text variables have been removed because of code readability improvement. 3. strconv.Itoa has been removed.

--- bottle/bottle.go
+++ bottle/bottle.go
@@ -6,25 +6,22 @@ import (
 	"strings"
 )
 
+const MAX_BOTTLES = 99
+
 func main() {
-	const MAX_BOTTLES = 99
-	var remainMessage = "%s bottles of beer"
-	var remainOnTheWall = "%s bottles of beer on the wall"
-	var takeMessage = "Take one down and pass it around"
-	var gotoStoreMessage = "Go to the store and buy some more"
 	var noMoreMessage = "No more"
 
 	for i := MAX_BOTTLES; i >= 0; i-- {
 		if i == 0 {
-			fmt.Printf(remainOnTheWall + ", " + remainMessage + ".\n", noMoreMessage, strings.ToLower(noMoreMessage))
-			fmt.Printf(gotoStoreMessage + ", " + remainMessage + ".\n\n", strconv.Itoa(MAX_BOTTLES))
+			fmt.Printf("%s bottles of beer on the wall, %s bottles of beer.\n", noMoreMessage, strings.ToLower(noMoreMessage))
+			fmt.Printf("Go to the store and buy some more, %v bottles of beer.\n\n", MAX_BOTTLES)
 		} else {
-			fmt.Printf(remainOnTheWall + ", " + remainMessage + ".\n", strconv.Itoa(i), strconv.Itoa(i))
+			fmt.Printf("%v bottles of beer on the wall, %v bottles of beer.\n", i, i)
 			var remainAfter string = strconv.Itoa(i-1)
 			if i == 1 {
 				remainAfter = strings.ToLower(noMoreMessage)
 			}
-			fmt.Printf(takeMessage + ", " + remainMessage + ".\n\n", remainAfter)
+			fmt.Printf("Take one down and pass it around, %s bottles of beer.\n\n", remainAfter)
 		}
 
 	}
