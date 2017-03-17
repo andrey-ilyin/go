7839cfa8d892750493fb7763f60076f665a19bc4
Andrey Ilyin <andrey.ilyin@ispringsolutions.com>
2017-03-17 20:52:45 +0300

    Function isAnagram has been implemented. Tests have been added.

---
+++ anagram/anagram_test.go
@@ -0,0 +1,25 @@
+package anagram
+
+import "testing"
+
+func TestAnagram(t *testing.T) {
+	cases := []struct {
+		first, second string
+		isAnagram     bool
+	}{
+		{"Dave Barry", "Ray Adverb", true},
+		{"Tom Marvolo Riddle", "I am Lord Voldemort", true},
+		{"просветитель", "терпеливость", true},
+		{"равновесие", "своенравие", true},
+
+		{"test", "fail", false},
+		{"ave Barry", "Ray Adverb", false},
+	}
+
+	for _, c := range cases {
+		got := isAnagram(c.first, c.second)
+		if got != c.isAnagram {
+			t.Errorf("isAnagram(%v, %v) == %v, want %v", c.first, c.second, got, c.isAnagram)
+		}
+	}
+}
